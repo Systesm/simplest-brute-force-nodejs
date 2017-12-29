c570bcd57f90750bc9059c5a173779d036cb6768
Quy Pham <phamhiepdan98@gmail.com>
2017-12-29 15:57:24 +0700

    Chinh sua dang nhap VTC

--- loginVTC.js
+++ loginVTC.js
@@ -1,12 +1,11 @@
 // Tao 1 form Login
 // Login nhieu se bi dinh captcha
-// Khi dinh captcha, get url https://header.vtcgame.vn/home/commonv2, tim den $("#key").val();
-// Lay key xong fetch dang nhap cung voi key vua get
+// Khi dinh captcha, get url https://header.vtcgame.vn/home/commonv2, tim den $("#key").val()
+// Lay key xong axios dang nhap cung voi key vua get
 // Kiem tra key bi het han hoac nhap nhieu lan, doi key khac nhu buoc 3
 
-const fetch = require("node-fetch");
-const cheerio = require('cheerio');
-const axios = require('axios');
+const cheerio = require('cheerio')
+const axios = require('axios')
 
 async function getKey() {
     try {
@@ -22,22 +21,47 @@ async function getKey() {
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Accept-Language': 'en-US,en;q=0.9',
             }
-        });
+        })
         const $ = cheerio.load(getUrl.data)
         return {
             key: $("#key").val(),
             headers: getUrl.headers
         }
     } catch (error) {
-        console.log(error)
+        throw new Error('Khong the get key: '+error)
     }
 
 }
 
+async function doLogin(user, pass, cookieSession, key) {
+    try {
+        let login = await axios({
+            method: 'POST',
+            url: 'https://header.vtcgame.vn/Handler/Process.ashx?act=Login',
+            headers: {
+                'Origin': 'https://vtcgame.vn',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'en-US, en; q=0.9',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36(KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Referer': 'https://vtcgame.vn/nap-vcoin/qua-the-cao.html',
+                'Cookie': 'ASP.NET_SessionId='+cookieSession,
+            },
+            data: 'conten=' + new Buffer(user).toString("base64") + '&value=' + new Buffer(pass).toString("base64") +'&capt=&hidverify=&isRemember=false&key='+key+'&otp=&otpType=1&returnURL=http%3A%2F%2Flocalhost%3A3955%2F:'
+        })
+        console.log(login.data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 (async () => {
     try {
-       let test =  await getKey()
-       console.log(test)
+        let getKeyandHeaders =  await getKey()
+        var cookieSession = new RegExp('[; ]ASP.NET_SessionId=([^\\s;]*)');
+            cookieSession = (' ' + getKeyandHeaders.headers["set-cookie"][0]).match(cookieSession);
+            cookieSession = unescape(cookieSession[1]);
+        await doLogin('systesm4', 'taolavip', cookieSession, getKeyandHeaders.key)
     } catch (error) {
         console.log(error)
     }
